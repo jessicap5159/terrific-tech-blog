f3fb180071133509617bbb8532b368815fdbb6d9
jessicap5159 <jessicaj5159@gmail.com>
2021-05-05 14:31:47 -0700

    created post routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,12 @@
-const homeRoutes = require('./home-routes.js');
 const router = require('express').Router();
 
+
+const homeRoutes = require('./home-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 router.use('/', homeRoutes);
+
+module.exports = router;
