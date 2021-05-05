e6911252665ceee1bbf59ad073bbb3d77eb200ad
jessicap5159 <jessicaj5159@gmail.com>
2021-05-05 14:22:12 -0700

    user routes written

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,18 +2,112 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+User.findAll()
+.then(dbUserData => res.json(dbUserData))
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
 
 // GET /api/users/1
-router.get('/:id', (req, res) => {});
+rrouter.get('/:id', (req, res) => {
+    User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 // POST /api/users
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
-// PUT /api/users/1
-router.put('/:id', (req, res) => {});
+  // LOGIN route
+router.post('/login', (req, res) => {
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        res.json({ user: dbUserData });
+    
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });  
+    });
 
+    // PUT /api/users/1
+router.put('/:id', (req, res) => {
+  
+    User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model { }
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+ }
 
 // define table columns and configuration
 User.init(
@@ -35,12 +39,23 @@ User.init(
         }
     },
     {
-
-        sequelize,
-        timestamps: false,
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+      
+            async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            }
+          },
+sequelize,
+    timestamps: false,
         freezeTableName: true,
-        underscored: true,
-        modelName: 'user'
+            underscored: true,
+                modelName: 'user'
     }
 );
 
