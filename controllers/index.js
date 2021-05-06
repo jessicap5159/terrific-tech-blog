097c67e74894848849a1e547a09dbe2a148d13c7
jessicap5159 <jessicaj5159@gmail.com>
2021-05-06 12:03:15 -0700

    updates to css

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 
+// const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/dashboard', dashboardRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
